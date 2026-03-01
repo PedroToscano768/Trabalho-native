3b556e888ddf166f403824a47581c9a13764138b
PedroToscano768 <pedro10toscano@gmail.com>
2026-03-01 16:07:25 -0300

    adicionando outros personagens ao exercicio-02

--- exercicio-02/App.tsx
+++ exercicio-02/App.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import { ScrollView, Text, StyleSheet, View, Image } from 'react-native';
+const mario = require('./assets/mario.png');
+const bowser = require('./assets/bowser.png');
+const leonardo = require('./assets/leonardo.png');
+const link = require('./assets/link.png');
+const luigi = require('./assets/luigi.png');
+const mlkFeio = require('./assets/mlk-feio.png');
+const imagens = [mario, bowser, leonardo, link, luigi, mlkFeio];
 
 export default function ExScrollView(){
   return(
     <ScrollView style={styles.scroll}>
-      {Array.from({ length: 20 }, (_, i) => (
-        <Text key={i} style={styles.item}>
-          Item {i + i}
-        </Text>
+      {imagens.map((imagemAtual, i) => (
+        <View key={i} style={styles.item}>
+          <Image source={imagemAtual} style={styles.imagemItem} />
+          <Text style={styles.text}>Personagem {i + 1}</Text>
+        </View>
       ))}
     </ScrollView>
   );
-
 }
-const mario = require('./assets/mario.png');
 
 const styles = StyleSheet.create({
   scroll: {
     flex: 1,
     backgroundColor: '#fafafa'
   },
-  item: {
+item: {
     padding: 20,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
+    borderBottomColor: '#ccc',
+  },
+  imagemItem: {
+    width: 300,
+    height: 300,
+    marginRight: 15,
+    resizeMode: 'contain',
+  },
+  text: {
+    fontSize: 18,
   }
 })
